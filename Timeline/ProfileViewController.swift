0b648c690d20183d4dddf5da1ff376d7a7dc00da
Natalielim <natalielim@Natalies-MacBook-Pro.local>
2016-08-03 14:35:37 -0700

    Profile added

---
+++ Timeline/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  GallereeHome
+//
+//  Created by Natalie Lim on 8/3/16.
+//  Copyright © 2016 Dianatalie. All rights reserved.
+//
+
+import UIKit
+
+class Profile: UIViewController {
+    
+}
